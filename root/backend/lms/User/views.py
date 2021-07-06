1e5c3161639acdeb6ca050c662392e7f199d8e30
cypherxx <vaishnavijahav@gmail.com>
2021-07-07 00:01:55 +0530

    teacher and student models added

--- root/backend/lms/User/views.py
+++ root/backend/lms/User/views.py
@@ -8,15 +8,11 @@ from .CustomAuth import CustomAuthenticationBackend
 import json,jwt
 from django.http.response import JsonResponse
 
-# def index(request):
-#     return render(request,'login.html')
-
 def login_token(request):
     login_data=request.body
     print(login_data)
     json_data = json.loads(login_data)
     print(json_data)
-
     user=CustomAuthenticationBackend.authenticate(CustomAuthenticationBackend,request,json_data['email'],json_data['password'])
     print("hi",user)
     if user is not None:
@@ -28,9 +24,8 @@ def login_token(request):
         return JsonResponse({
         'token': jwt_t ,
         'user': user_data
-    })
+        })
     else:
-        # return HttpResponse("Invalid Credentials")
         return JsonResponse({'msg':'Error'})
 
 
