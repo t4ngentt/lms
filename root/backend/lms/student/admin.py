22cd47e58422823726a4a99253f439006a337b98
Sumit <ssmahajan@mitaoe.ac.in>
2021-07-09 12:18:40 +0530

    Student_teacher course & grps created

--- root/backend/lms/student/admin.py
+++ root/backend/lms/student/admin.py
@@ -1,12 +1,12 @@
-# from django.contrib import admin
-# from .models import Student_Info,Student_Group,Student_Course,Student_Current_Info,Project,Student_Profile,Student_Project,Student_Documents,Student_Quiz,Student_Quiz_Answers,Student_Selected_Quiz,Student_Selected_Quiz_Answers,Student_Result
-# # Register your models here.
+from django.contrib import admin
+from .models import Student_Group,Student_Course,Student_Current_Info
+# Register your models here.
 
 
-# admin.register(Student_Course)
+admin.site.register(Student_Course)
 # admin.register(Student_Info)
-# admin.register(Student_Group)
-# admin.register(Student_Current_Info)
+admin.site.register(Student_Group)
+admin.site.register(Student_Current_Info)
 # admin.register(Student_Profile)
 # admin.register(Student_Project)
 # admin.register(Student_Documents)
--- root/backend/lms/teacher/models.py
+++ root/backend/lms/teacher/models.py
@@ -1,51 +1,51 @@
 from django.db import models
 from User.models import User,School,Group_Course,Branch
-from student.models import Student_Info
+# from student.models import Student_Info
 # Create your models here.
 
-# class Teacher_Info(models.Model):
-#     teacher_id = models.BigAutoField(primary_key=True)
-#     teacher_name = models.CharField(max_length=100)
-#     school = models.ForeignKey(School,on_delete=models.CASCADE,verbose_name='school_fk')
-#     branch = models.ForeignKey(Branch,on_delete=models.CASCADE,verbose_name='branch_fk')
-#     user = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='user_fk')
+# # class Teacher_Info(models.Model):
+# #     teacher_id = models.BigAutoField(primary_key=True)
+# #     teacher_name = models.CharField(max_length=100)
+# #     school = models.ForeignKey(School,on_delete=models.CASCADE,verbose_name='school_fk')
+# #     branch = models.ForeignKey(Branch,on_delete=models.CASCADE,verbose_name='branch_fk')
+# #     user = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='user_fk')
 
-#     class Meta:
-#         db_table = 'TEACHER_INFO'
-#         verbose_name = 'teacher_info'
-#         verbose_name_plural = 'teacher_infos'
+# #     class Meta:
+# #         db_table = 'TEACHER_INFO'
+# #         verbose_name = 'teacher_info'
+# #         verbose_name_plural = 'teacher_infos'
 
-# class Skills(models.Model):
-#     skill = models.CharField(max_length=100)
-#     student = models.ForeignKey(Student_Info,on_delete=models.CASCADE,verbose_name='student_info_fk')
+# # class Skills(models.Model):
+# #     skill = models.CharField(max_length=100)
+# #     student = models.ForeignKey(Student_Info,on_delete=models.CASCADE,verbose_name='student_info_fk')
+# #     teacher = models.ForeignKey(Teacher_Info,on_delete=models.CASCADE,verbose_name='teacher_info_fk')
+# #     school = models.ForeignKey(School,on_delete=models.CASCADE,verbose_name='school_fk')
+# #     branch = models.ForeignKey(Branch,on_delete=models.CASCADE,verbose_name='branch_fk')
+# #     class Meta:
+# #         db_table = 'SKILLS'
+# #         verbose_name = 'skills'
+# #         verbose_name_plural = 'skills'
+
+# class Teacher_Profile(models.Model):
 #     teacher = models.ForeignKey(Teacher_Info,on_delete=models.CASCADE,verbose_name='teacher_info_fk')
+#     bio = models.TextField()
 #     school = models.ForeignKey(School,on_delete=models.CASCADE,verbose_name='school_fk')
 #     branch = models.ForeignKey(Branch,on_delete=models.CASCADE,verbose_name='branch_fk')
+#     # certificates
+#     # experience
+#     # achievements
+#     # qualifications
+#     # interests
+#     # social_media
+
 #     class Meta:
-#         db_table = 'SKILLS'
-#         verbose_name = 'skills'
-#         verbose_name_plural = 'skills'
-
-class Teacher_Profile(models.Model):
-    teacher = models.ForeignKey(Teacher_Info,on_delete=models.CASCADE,verbose_name='teacher_info_fk')
-    bio = models.TextField()
-    school = models.ForeignKey(School,on_delete=models.CASCADE,verbose_name='school_fk')
-    branch = models.ForeignKey(Branch,on_delete=models.CASCADE,verbose_name='branch_fk')
-    # certificates
-    # experience
-    # achievements
-    # qualifications
-    # interests
-    # social_media
-
-    class Meta:
-        db_table = 'TEACHER_PROFILE'
-        verbose_name = 'teacher_profile'
-        verbose_name_plural = 'teacher_profiles'
+#         db_table = 'TEACHER_PROFILE'
+#         verbose_name = 'teacher_profile'
+#         verbose_name_plural = 'teacher_profiles'
 
 class Teacher_Course(models.Model):
     teacher_course_id = models.BigAutoField(primary_key=True)
-    teacher = models.ForeignKey(Teacher_Info,on_delete=models.CASCADE,verbose_name='teacher_info_fk')
+    teacher = models.ForeignKey(User,on_delete=models.CASCADE,verbose_name='user_fk')
     group_course = models.ForeignKey(Group_Course,on_delete=models.CASCADE,verbose_name='group_course_fk')
     school = models.ForeignKey(School,on_delete=models.CASCADE,verbose_name='school_fk')
     branch = models.ForeignKey(Branch,on_delete=models.CASCADE,verbose_name='branch_fk') 
