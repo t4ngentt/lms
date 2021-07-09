28e79f9f4072a8ef6dc0cc95217614967c4c7125
Sumit <ssmahajan@mitaoe.ac.in>
2021-07-09 22:24:47 +0530

    conflicts issue

--- root/backend/lms/student/views.py
+++ root/backend/lms/student/views.py
@@ -1,16 +1,15 @@
-from .serializers import Student_Course_Serializer, Student_Group_Serializer
+from .serializers import Student_Group_Serializer
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin
-from .models import Student_Course, Student_Group
+from .models import Student_Group
 # Create your views here.
 
 class Student_Group(GenericAPIView,ListModelMixin):
-
+    
     queryset = Student_Group.objects.filter(student="0120190202")
     serializer_class = Student_Group_Serializer
-    queryset = Student_Course.objects.filter(student="0120190202")
-    serializer_class = Student_Course_Serializer
+
     def get(self,request,*args,**kwargs):
         return self.list(request,*args,**kwargs)
 
