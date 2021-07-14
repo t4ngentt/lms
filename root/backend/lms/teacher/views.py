a41e6e0cdcf15305661b0de45a6900551b7b1469
cypherxx <vaishnavijahav@gmail.com>
2021-07-14 23:49:32 +0530

    views.py teacher changed

--- root/backend/lms/teacher/views.py
+++ root/backend/lms/teacher/views.py
@@ -17,12 +17,11 @@ import pandas as pd
 # Create your views here.
 
 class Teacher_Course_Names(APIView):
-    def get(self, request):
+    def post(self, request):
         # if token_authentication(request):
-            #teacher_id = (json.loads(request.body))['teacher_id']
-            #print(teacher_id)
-            print("hello")
-            querylist = Teacher_Course.objects.filter(teacher='0120190203').values()
+            teacher_id = (json.loads(request.body))['teacher_id']
+            print(teacher_id)
+            querylist = Teacher_Course.objects.filter(teacher=teacher_id).values()
             print(querylist)
             group_course_fk = []
             for j in querylist:
@@ -35,23 +34,5 @@ class Teacher_Course_Names(APIView):
                 y=Course.objects.get(course_id=j.course_id)
                 j.course_id=y.course_name
             serializer = Teacher_Course_Serializer(queryset, many=True)
-            tmp_data=pd.read_csv('teacher/sample.csv')
-            df= DataFrame(tmp_data)
-
-            User_data=[]
-            for index,row in df.iterrows():
-                x=User(
-                    user_id=row['user_id'],
-                    f_name=row['f_name'],
-                    l_name=row['l_name'],
-                    email=row['email'],
-                    staff=row['staff'],
-                    is_active=True,
-                    admin=row['admin'],
-                    role=row['role'],
-                )
-                x.set_password("Vaishu@22")
-                User_data.append(x)
-            User.objects.bulk_create(User_data)
             return Response(serializer.data)
 
