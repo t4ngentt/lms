358b95dd2655b4c34b8632837c65ad8018e3bc43
cypherxx <vaishnavijahav@gmail.com>
2021-07-26 13:57:36 +0530

    forward slashes

--- root/backend/lms/assignment/serializers.py
+++ root/backend/lms/assignment/serializers.py
@@ -17,15 +17,15 @@ class Assignment_Detail_Serializer(serializers.ModelSerializer):
         fields = ["assignment_id","title","description","min_marks","max_marks","post_date","due_date","visibility","file_path"]
 
     def get_file_path(self,obj):
-            folder_path =str(BASE_DIR)+'\\assignments\\'+str(obj.assignment_id)
+            folder_path =str(BASE_DIR)+'/assignments/'+str(obj.assignment_id)
             assignments = os.listdir(folder_path)
             submission_file_path=[]
             for j in assignments:
-                submission_file_path.append(folder_path+"\\"+j)
+                submission_file_path.append(folder_path+"/"+j)
             return submission_file_path
 
 
-            
+
 class Assignment_Submission_Serializer(serializers.ModelSerializer):
 
     file_path = serializers.SerializerMethodField()
@@ -35,11 +35,11 @@ class Assignment_Submission_Serializer(serializers.ModelSerializer):
         fields = ['assignment_submission_id','assignment_id','prn','name','submit_time','file_path',]
     
     def get_file_path(self,obj):
-            folder_path =str(BASE_DIR)+'\\studentdata\\'+str(obj.prn.user_id)+'\\assignments\\'+str(obj.assignment_id.assignment_id)
+            folder_path =str(BASE_DIR)+'/studentdata/'+str(obj.prn.user_id)+'/assignments/'+str(obj.assignment_id.assignment_id)
             assignments = os.listdir(folder_path)
             submission_file_path=[]
             for j in assignments:
-                submission_file_path.append(folder_path+"\\"+j)
+                submission_file_path.append(folder_path+"/"+j)
             return submission_file_path
     
     def get_name(self,queryset):
